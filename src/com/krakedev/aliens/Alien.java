42a1ade7ec71905eb7c6bf4aa5cb4dc35952ce07
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-08-13 00:33:47 -0500

    Agregar constructor de Alien con validación de tamaño y cálculo de precios

--- src/com/krakedev/aliens/Alien.java
+++ src/com/krakedev/aliens/Alien.java
@@ -9,4 +9,19 @@ public class Alien {
     private double precioExtremidad;
     private double precioOjo;
     private double precioCuerpo;
+
+    public Alien(int tamanio, String color) {
+        if (tamanio < 5) {
+            tamanio = 5;
+        } else if (tamanio > 30) {
+            tamanio = 30;
+        }
+
+        this.tamanio = tamanio;
+        this.color = color;
+
+        this.precioCuerpo = tamanio * 0.20;
+        this.precioExtremidad = tamanio * 0.10;
+        this.precioOjo = tamanio * 0.05;
+    }
 }
